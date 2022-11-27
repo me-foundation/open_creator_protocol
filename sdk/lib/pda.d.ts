444a63f1972754ace2dbf28c343bcb6adb01b991
Rex <rex@magiceden.io>
2022-11-27 10:31:49 -0800

    Fix .gitignore

--- sdk/lib/pda.d.ts
+++
@@ -1,5 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-export declare const CMT_PROGRAM: PublicKey;
-export declare const findPolicyPk: (uuid: PublicKey) => PublicKey;
-export declare const findMintStatePk: (mint: PublicKey) => PublicKey;
-export declare const findFreezeAuthorityPk: (policy: PublicKey) => PublicKey;
